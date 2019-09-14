6ccbf9348f7ccee5a3741f2faaf27e4dc2d41a34
muriloconceicao <daconceicaomurilo@gmail.com>
2019-09-13 23:43:42 -0300

    First commit.

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_integration/Setup/signIn.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
